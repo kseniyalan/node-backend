c26c3c1857567484da2d5e8e38b92ff0b3865eeb
Ksenia <k.lanetskaya@gmail.com>
2020-07-22 19:31:58 +0300

    Роуты подключены к серверу

--- create_server.js
+++ create_server.js
@@ -5,10 +5,14 @@ const Koa = require('koa');
 const logger = require('koa-logger');
 const cors = require('@koa/cors');
 
+const routes = require('./routes/routes');
+
 const app = new Koa();
 app.use(logger());
 app.use(cors());
 
 app.proxy = true;
+
+app.use(routes.routes()).use(routes.allowedMethods());
 
 module.exports = app;
