7a185bd5d201488892e2ec5619d3b83d26729d3b
Ksenia <k.lanetskaya@gmail.com>
2020-07-22 17:42:47 +0300

    Сиды фруктов

---
+++ dao/seeders/20200722-fruits-demo.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('fruits', [
+      {
+        id: 111,
+        name: 'Яблоко',
+        amount: 3,
+        eaten: false,
+        created_at: new Date(new Date().setMinutes(0)),
+      },
+      {
+        id: 222,
+        name: 'Груша',
+        amount: 1,
+        eaten: false,
+        created_at: new Date(new Date().setMinutes(0)),
+      },
+      {
+        id: 333,
+        name: 'Апельсин',
+        amount: 2,
+        eaten: false,
+        created_at: new Date(new Date().setMinutes(0)),
+      },
+      {
+        id: 444,
+        name: 'Грейпфрут',
+        amount: 1,
+        eaten: false,
+        created_at: new Date(new Date().setMinutes(0)),
+      },
+      {
+        id: 555,
+        name: 'Банан',
+        amount: 5,
+        eaten: true,
+        created_at: new Date(new Date().setMinutes(0)),
+      },
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('fruits', null, {});
+  },
+};
