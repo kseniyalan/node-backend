ea8fcbe2fb5ef762b2ba3a2608ab26deb1d51010
Ksenia <k.lanetskaya@gmail.com>
2020-07-24 18:27:25 +0300

    Роут для загрузки файлов

---
+++ controllers/upload/index.js
@@ -0,0 +1,3 @@
+exports.Upload = async (ctx) => {
+
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const Router = require('koa-router');
 const koaBody = require('koa-body');
+const path = require('path');
 
 const FruitsController = require('../controllers/fruits');
+const UploadController = require('../controllers/upload');
 const AuthMiddleware = require('../controllers/middlewares/middleware');
+
+const config = require('../config');
 
 const router = new Router({
   prefix: `/api`,
@@ -23,5 +27,28 @@ router.post('/fruits', AuthMiddleware, koaBody(), FruitsController.CreateFruit);
 
 //Удалить фрукт
 router.delete('/fruits/:id', AuthMiddleware, FruitsController.DeleteFruit);
+
+//Загрузка картинок
+router.post(
+  '/files/upload',
+  AuthMiddleware,
+  koaBody({
+    multipart: true,
+
+    formidable: {
+      uploadDir: path.join(
+        __dirname,
+        '..',
+        config.static_directory,
+        config.temp_directory,
+      ),
+      keepExtensions: true,
+      multiples: false,
+      maxFileSize: 1024 * 1024 * config.max_file_size,
+      maxFields: 1,
+    },
+  }),
+  UploadController.Upload,
+);
 
 module.exports = router;
