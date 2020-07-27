985eab3b53a647337fd37c76749387c627d3c3d3
Ksenia <k.lanetskaya@gmail.com>
2020-07-27 23:50:55 +0300

    Формирование пути картинки

--- controllers/upload/index.js
+++ controllers/upload/index.js
@@ -1,11 +1,61 @@
+const fs = require('fs');
+const util = require('util');
+const UploadModule = require('./modules');
 
 const ApiModels = {
   image: require('../../api-models/image'),
 };
 
+const { Image } = require('../../dao');
+
+const fsUnlink = util.promisify(fs.unlink);
 
 exports.Upload = async (ctx) => {
   const files = ctx.request.files;
   let image = {};
+
+  if (!files) {
+    return Error(ctx, 400, 'Файлы не найдены');
+  }
+
+  for (let key in files) {
+    if (key !== 'image') await fsUnlink(files[key].path);
+  }
+
+  if (!files.image) {
+    return Error(ctx, 400, 'Не удалось получить файл "image"');
+  }
+  files.image.name = files.image.path.split('_')[1];
+
+  try {
+    const {
+      originalOrientation,
+      meta,
+      previewBuffer,
+    } = await UploadModule.HandleImage({ files: files });
+
+    image = {
+      height:
+        originalOrientation && originalOrientation > 4
+          ? meta.width
+          : meta.height,
+      width:
+        originalOrientation && originalOrientation > 4
+          ? meta.height
+          : meta.width,
+      preview: `data:image/jpeg;base64,${previewBuffer.toString('base64')}`,
+      src: files.image.path,
+    };
+
+    console.log('Path: ', files.image.path);
+    console.log('Image: ', files.image);
+  } catch (err) {
+      console.log(err);
+      await fsUnlink(files.image.path);
+    return Error(ctx, 400, 'Не удалось обработать файл "image"');
+  }
+
+  image = await Image.create(image);
+
   return (ctx.body = ApiModels.image(ctx, image));
 };
---
+++ controllers/upload/modules.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const util = require('util');
+const sharp = require('sharp');
+
+const fsreadFile = util.promisify(fs.readFile);
+
+const config = require('../../config');
+
+exports.HandleImage = async ({ files }) => {
+  const originalBuffer = await fsreadFile(files.image.path);
+  const savedObject = sharp(originalBuffer);
+  const meta = await savedObject.metadata();
+
+  const originalOrientation = meta.orientation;
+
+  //Обработка поворотов картинки
+  if (originalOrientation) { 
+    switch (originalOrientation) {
+      case 2:
+        savedObject.flip();
+        break;
+      case 3:
+        savedObject.rotate(180);
+        break;
+      case 4:
+        savedObject.flop();
+        break;
+      case 5:
+        savedObject.flip().rotate(270);
+        break;
+      case 6:
+        savedObject.rotate(90);
+        break;
+      case 7:
+        savedObject.flip().rotate(90);
+        break;
+      case 8:
+        savedObject.rotate(270);
+    }
+  }
+
+  const previewBuffer = await savedObject
+    .clone()
+    .resize(config.preview_size, config.preview_size)
+    .toBuffer();
+  await savedObject.toFormat('jpg').withMetadata().toFile(files.image.path);
+
+  return {
+    originalOrientation,
+    meta,
+    previewBuffer,
+  };
+};
